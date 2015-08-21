e66f88c12b43b38f29469dd96ef6028682f9eb4e
jshepardy <julian.tonte@gmail.com>
2015-08-21 14:43:59 +0200

    update

--- js/presentation.js
+++ js/presentation.js
@@ -1,11 +1,13 @@
-angular.module('RockDogPresentation', ['pascalprecht.translate'])
+angular.module('RockDogPresentation', ['pascalprecht.translate', 'ngSanitize'])
 .config(function ($translateProvider) {
   //$translateProvider.useSanitizeValueStrategy('sanitize');
   console.log(window.RDLangEN);
+  $translateProvider.useSanitizeValueStrategy('sanitize');
   $translateProvider.translations("en_US", window.RDLangEN);
   $translateProvider.translations("nl", window.RDLangNL);
   $translateProvider.preferredLanguage("en_US");
   $translateProvider.determinePreferredLanguage();
+  $translateProvider.fallbackLanguage('en_US');
 })
 .controller("Rockdog", function($scope) {
   $(document).ready(function(){
