b0f82ec410a9e36936a39cf49fb28b3bb3901607
Julian Tonte <julian.tonte@gmail.com>
2015-08-21 20:17:38 +0200

    prototype

--- js/presentation.js
+++ js/presentation.js
@@ -10,9 +10,40 @@ angular.module('RockDogPresentation', ['pascalprecht.translate', 'ngSanitize'])
   $translateProvider.fallbackLanguage('en_US');
 })
 .controller("Rockdog", function($scope) {
-  $(document).ready(function(){
-    setTimeout(function(){
-      $(".navigate-up").html($("<span class='contact-label up'>Contact</span>"));
-    }, 300)
-  });
+  Reveal.slide(0,0,0);
+ /* $scope.txtRight = "contact";
+  $scope.txtBottom = "presentation";
+  Reveal.addEventListener( 'slidechanged', function( event ) {
+    $scope.$apply(function() {
+      $scope.txtRight = $(event.currentSlide).attr("data-nav-right");
+      $scope.txtLeft = $(event.currentSlide).attr("data-nav-left");
+      $scope.txtUp = $(event.currentSlide).attr("data-nav-up");
+      $scope.txtBottom = $(event.currentSlide).attr("data-nav-bottom");
+    });
+    angular.forEach($('.nav-indicator>strong'), function(indicator) {
+      $(indicator).removeClass('swing');
+      $(indicator).addClass('swing');
+      setTimeout(function() {
+        $(indicator).removeClass('swing');
+      },3000);
+    });
+  });*/
+
+});
+
+Reveal.initialize({
+    // The "normal" size of the presentation, aspect ratio will be preserved
+    // when the presentation is scaled to fit different resolutions. Can be
+    // specified using percentage units.
+    width: 1000,
+    height: 600,
+    parallaxBackgroundImage: '/public/Frame1.png',
+    parallaxBackgroundSize: '2330px 1739px',
+    // Factor of the display size that should remain empty around the content
+    margin: 0.15,
+
+    // Bounds for smallest/largest possible scale to apply to content
+    minScale: 0.2,
+    maxScale: 1.5
+
 });
